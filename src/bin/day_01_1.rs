542534c70cd86f75cbded068954373e14d2a064d
wyvernbw <iamwyvern511@gmail.com>
2024-01-04 23:45:56 +0200

    change day_01_1 to use nom

--- src/bin/day_01_1.rs
+++ src/bin/day_01_1.rs
@@ -1,18 +1,27 @@
+use nom::{character::complete::anychar, combinator::map, multi::many0, IResult};
+
 const INPUT: &str = include_str!("../day_01_1_input.txt");
 
 pub fn main() {
     advent_of_code_2023::initialize();
-    let res = INPUT
+    let result = INPUT
         .lines()
-        .flat_map(|line| {
-            let iter = line.chars().filter(|c| c.is_numeric());
-            iter.clone()
-                .take(1)
-                .chain(iter.rev().take(1))
-                .filter_map(|c| c.to_digit(10))
-                .reduce(|acc, el| acc * 10 + el)
-        })
+        .map(calibration)
         .inspect(|el| tracing::info!(el))
         .sum::<u32>();
-    tracing::info!(res)
+    tracing::info!(?result)
+}
+
+fn digits(input: &str) -> IResult<&str, Vec<u32>> {
+    many0(map(anychar, |ch| ch.to_digit(10)))(input)
+        .map(|(input, digits)| (input, digits.into_iter().flatten().collect()))
+}
+
+fn calibration(input: &str) -> u32 {
+    let (_, digits) = digits(input).expect("digits");
+    let (first, last) = digits
+        .first()
+        .zip(digits.last())
+        .expect("expected at least one digit");
+    first * 10 + last
 }
